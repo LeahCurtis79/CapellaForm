39ed25ab7f4fdd09ba45f784f7e97dde4533e933
LeahCurtis79 <80548573+LeahCurtis79@users.noreply.github.com>
2021-06-07 20:06:59 -0500

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,12 @@
+(function() {
+	[...document.querySelectorAll("img")].forEach(item=>{
+		item.onmouseover = () => { 
+			item.classList.add('over'); 
+		}
+		
+		item.onmouseout = () => { 
+			item.classList.remove('over'); 
+		}
+	});
+	document.querySelector("img").classList.add('first');
+})();
